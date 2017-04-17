387d86329c57bb336f368289a981f46b1afeb423
Mafe Avendaño <mafe.ud@gmail.com>
2017-04-17 14:27:25 -0500

    Se agregan servicios de consulta de periodo anterior y consulta de % cursado

--- app/scripts/controllers/materias_posgrado/solicitar_asignaturas.js
+++ app/scripts/controllers/materias_posgrado/solicitar_asignaturas.js
@@ -8,18 +8,59 @@
  * Controller of the poluxClienteApp
  */
 angular.module('poluxClienteApp')
-  .controller('MateriasPosgradoSolicitarAsignaturasCtrl', function (poluxMidRequest) {
+  .controller('MateriasPosgradoSolicitarAsignaturasCtrl', function (poluxMidRequest, academicaRequest, $scope) {
     var ctrl = this;
+    $scope.codigo="20092020008";
 
-    ctrl.estudiante={
-      "Codigo": "20102020009",
-      "Nombre": "PARRA FUENTES FABIO ANDRES",
-      "Tipo": "POSGRADO"
-    };
-    ctrl.modalidad="MATERIAS POSGRADO";
+    $scope.$watch("codigo",function() {
 
-    poluxMidRequest.post("disponibilidad/Registrar?tdominio=2", self.colegio).then(function(response){
-        console.log(response);
-        ctrl.validar= response.data;
+      academicaRequest.periodoAnterior().then(function(periodoAnterior){
+
+        var parametros = {
+          "codigo": $scope.codigo,
+          //periodo anterior
+          'ano' : periodoAnterior[0].APE_ANO,
+          'periodo' :periodoAnterior[0].APE_PER
+        };
+
+        academicaRequest.promedioEstudiante(parametros).then(function(response2){
+
+          if(response2){
+            //porcentaje cursado
+            var parametros2 = {
+              "codigo": parametros.codigo
+            };
+
+            academicaRequest.porcentajeCursado(parametros).then(function(response3){
+              console.log(response3);
+
+              ctrl.estudiante={
+                "Codigo": parametros.codigo,
+                "Nombre": response2[0].NOMBRE,
+                "Modalidad": 6,
+                "Tipo": "POSGRADO",
+                "PorcentajeCursado": response3,
+                "Promedio": response2[0].PROMEDIO,
+                "Rendimiento": "0"+response2[0].REG_RENDIMIENTO_AC,
+                "Estado": response2[0].EST_ESTADO_EST,
+                "Nivel": response2[0].TRA_NIVEL,
+                "TipoCarrera": response2[0].TRA_NOMBRE
+
+              };
+
+              console.log(ctrl.estudiante);
+              ctrl.modalidad="MATERIAS POSGRADO";
+
+              poluxMidRequest.post("disponibilidad/Registrar", ctrl.estudiante).then(function(response){
+                  console.log(response);
+                  ctrl.validar= response.data;
+              });
+            });
+          }
+
+        });
+
+      });
     });
+
   });
