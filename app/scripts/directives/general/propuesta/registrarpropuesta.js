ea2c1f9ef0c3fc3c96203b46ebbd6e706bde5efa
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-05-24 14:29:31 -0500

    resolviendo pullRequest David

--- app/scripts/directives/general/propuesta/registrarpropuesta.js
+++ app/scripts/directives/general/propuesta/registrarpropuesta.js
@@ -7,7 +7,7 @@
  * # registrarPropuesta
  */
 angular.module('poluxClienteApp')
-  .directive('registrarPropuesta', function (academicaRequest, poluxRequest) {
+  .directive('registrarPropuesta', function(academicaRequest, poluxRequest) {
     return {
       restrict: 'E',
       scope: {
@@ -19,11 +19,11 @@ angular.module('poluxClienteApp')
         dialparent: '&'
       },
       templateUrl: 'views/directives/general/propuesta/registrar-propuesta.html',
-      controller: function ($scope) {
+      controller: function() {
         var self = this;
         self.modSeleccionada = "";
         self.buttonDirective = "Aceptar";
-        poluxRequest.get("modalidad", "").then(function (response) {
+        poluxRequest.get("modalidad", "").then(function(response) {
           self.modalidad = response.data;
         });
         self.estado = false;
@@ -32,12 +32,12 @@ angular.module('poluxClienteApp')
         self.registro_TG = [];
         self.areasTG = [];
         /**/
-        self.dial = function (doc) {
+        self.dial = function(doc) {
           console.log("funciona llamado de directiva hija a directiva padre");
           console.log(" " + doc.enlace);
-        }
+        };
 
-        self.asignarEstudiante = function (codEstudiante) {
+        self.asignarEstudiante = function(codEstudiante) {
           console.log(codEstudiante);
           codEstudiante = parseInt(codEstudiante);
           self.parametros = $.param({
@@ -46,18 +46,20 @@ angular.module('poluxClienteApp')
 
         };
 
-        self.estadoboton = function (estadoboton) {
+        self.estadoboton = function(estadoboton) {
           self.menucreacion = !self.menucreacion;
-          if (estadoboton == "Aceptar") {
+          if (estadoboton === "Aceptar") {
             self.buttonDirective = "Volver al Formulario";
             return false;
+          } else {
+            self.buttonDirective = "Aceptar";
+            return true;
           }
-          else { self.buttonDirective = "Aceptar"; return true; }
-        }
+        };
 
         /*self.guardar:
         guarda temporalmente los registros necesarios para el registro de la propuesta*/
-        self.guardar = function (doc, estudiante, idModalidad) {
+        self.guardar = function(doc, estudiante, idModalidad) {
           var codEstudiante, idTrabajoGrado;
           codEstudiante = parseInt(estudiante);
           console.log("titulo: " + doc.titulo + " , modalidad: " + idModalidad);
@@ -100,15 +102,15 @@ angular.module('poluxClienteApp')
 
         /**/
 
-        self.preguardarTG = function (title, mod) {
-          self.registro_TG.push(
-            {
-              "Distincion": "ninguno", //modificar base de datos para agregar check de "ninguno"
-              "Etapa": "solicitud tg",
-              "IdModalidad": { "Id": mod },
-              "Titulo": title
-            }
-          );
+        self.preguardarTG = function(title, mod) {
+          self.registro_TG.push({
+            "Distincion": "ninguno", //modificar base de datos para agregar check de "ninguno"
+            "Etapa": "solicitud tg",
+            "IdModalidad": {
+              "Id": mod
+            },
+            "Titulo": title
+          });
 
 
 
@@ -118,21 +120,21 @@ angular.module('poluxClienteApp')
 
         /**/
 
-        self.guardarTG = function (data, estudiante, doc) {
+        self.guardarTG = function(data, estudiante, doc) {
           var idEstudianteTG;
           self.TGregistrado = [];
-          poluxRequest.post("trabajo_grado", data[0]).then(function (responseTG) {
+          poluxRequest.post("trabajo_grado", data[0]).then(function(responseTG) {
             console.log("respuesta del post del trabajo de grado" + responseTG.data.Id);
             self.estudiante_TG = self.preguardarEstudianteTG(responseTG.data.Id, estudiante);
             idEstudianteTG = self.guardarestudianteTG(self.estudiante_TG[0]);
             self.docregistrado = self.preguardarDocumento(doc.titulo, doc.resumen, doc.enlace);
             console.log("doc: " + self.docregistrado[0]);
-            poluxRequest.post("documento", self.docregistrado[0]).then(function (responseDoc) {
+            poluxRequest.post("documento", self.docregistrado[0]).then(function(responseDoc) {
               console.log("data.Id: " + responseDoc.data.Id);
               self.guardarDocumentoTG(responseDoc.data.Id, responseTG.data.Id);
               poluxRequest.get("documento", $.param({
                 query: "Id:" + responseDoc.data.Id
-              })).then(function (response) {
+              })).then(function(response) {
                 self.registroDocumento = response.data;
               });
             });
@@ -140,52 +142,52 @@ angular.module('poluxClienteApp')
         };
 
         /**/
-        self.guardarRegistro = function () {
+        self.guardarRegistro = function() {
           self.registroDocumento = [];
 
-          poluxRequest.post("documento", self.docregistrado[0]).then(function (response) {
+          poluxRequest.post("documento", self.docregistrado[0]).then(function(response) {
             console.log("data.Id: " + response.data.Id);
             self.guardarDocumentoTG(response.data.Id, self.TGregistrado[0]);
             poluxRequest.get("documento", $.param({
               query: "Id:" + response.data.Id
-            })).then(function (response) {
+            })).then(function(response) {
               self.registroDocumento = response.data;
             });
 
-          poluxRequest.get("areas_docente",$.param({
-            query: "IdAreaConocimiento:"+areaSeleccionada
-          })).then(function(response){
-            if (response.data==null)
-              self.docentesArea=[];
-            else
-              self.docentesArea=self.unificarDocentes(response.data);
+            poluxRequest.get("areas_docente", $.param({
+              query: "IdAreaConocimiento:" + areaSeleccionada
+            })).then(function(response) {
+              if (response.data === null) {
+                self.docentesArea = [];
+              } else {
+                self.docentesArea = self.unificarDocentes(response.data);
+              }
+            });
           });
         };
         /*self.guardar:
         guarda temporalmente los registros necesarios para el registro de la propuesta*/
-        self.guardar=function(doc,docenteSeleccionado,estudiante){
+        self.guardar = function(doc, docenteSeleccionado, estudiante) {
           var codEstudiante;
-          codEstudiante=parseInt(estudiante);
-          self.estado= true;
-          self.docregistrado=[];
-          self.TGregistrado=[];
-          self.areas_TG=[];
-          self.vinculaciondocente=[];
-          self.estudiante_TG=[];
-          self.docTG=[];
+          codEstudiante = parseInt(estudiante);
+          self.estado = true;
+          self.docregistrado = [];
+          self.TGregistrado = [];
+          self.areas_TG = [];
+          self.vinculaciondocente = [];
+          self.estudiante_TG = [];
+          self.docTG = [];
           console.log(doc.enlace);
-          poluxRequest.get("estudiante_tg",$.param({
-            query:"CodigoEstudiante:"+codEstudiante
-          })).then(function(response){
-            console.log("respuesta: "+response.data[0].IdTrabajoGrado.Id);
-            self.TGregistrado.push(
-              {
-                "Id": response.data[0].IdTrabajoGrado.Id
-              }
-            );
-            self.areas_TG=self.preguardarAreasTG(self.TGregistrado);
-            self.vinculaciondocente=self.preguardarVinculacion(self.TGregistrado,docenteSeleccionado);
-            self.estudiante_TG=self.preguardarEstudianteTG(self.TGregistrado,estudiante);
+          poluxRequest.get("estudiante_tg", $.param({
+            query: "CodigoEstudiante:" + codEstudiante
+          })).then(function(response) {
+            console.log("respuesta: " + response.data[0].IdTrabajoGrado.Id);
+            self.TGregistrado.push({
+              "Id": response.data[0].IdTrabajoGrado.Id
+            });
+            self.areas_TG = self.preguardarAreasTG(self.TGregistrado);
+            self.vinculaciondocente = self.preguardarVinculacion(self.TGregistrado, docenteSeleccionado);
+            self.estudiante_TG = self.preguardarEstudianteTG(self.TGregistrado, estudiante);
           });
 
           self.asignarAreasTG(self.areas_TG);
@@ -193,105 +195,100 @@ angular.module('poluxClienteApp')
         };
 
         /**/
-        self.preguardarDocumento = function (titulo, resumen, enlace) {
-          self.docregistrado.push(
-            {
-              "Titulo": titulo,
-              "Resumen": resumen,
-              "Enlace": enlace,
-              "IdTipoDocumento": {
-                "Id": 1,
-                "IdCategoria": { //puede ser nulo
-                  "Id": 1
-                }
+        self.preguardarDocumento = function(titulo, resumen, enlace) {
+          self.docregistrado.push({
+            "Titulo": titulo,
+            "Resumen": resumen,
+            "Enlace": enlace,
+            "IdTipoDocumento": {
+              "Id": 1,
+              "IdCategoria": { //puede ser nulo
+                "Id": 1
               }
             }
-          );
+          });
           return self.docregistrado;
         };
         /**/
-        self.preguardarAreasTG = function (dataIdTG) {
+        self.preguardarAreasTG = function(dataIdTG) {
           for (var i = 0; i < self.nuevaArea.length; i++) {
-            self.areas_TG.push(
-              {
-                "IdAreaConocimiento": {
-                  "Id": self.nuevaArea[i].Id,
-                  "Nombre": self.nuevaArea[i].Nombre
-                },
-                "IdTrabajoGrado": dataIdTG[0]
-              }
-            );
+            self.areas_TG.push({
+              "IdAreaConocimiento": {
+                "Id": self.nuevaArea[i].Id,
+                "Nombre": self.nuevaArea[i].Nombre
+              },
+              "IdTrabajoGrado": dataIdTG[0]
+            });
           }
           return self.areas_TG;
 
         };
         /**/
-        self.preguardarVinculacion = function (dataIdTG, docenteSeleccionado) {
+        self.preguardarVinculacion = function(dataIdTG, docenteSeleccionado) {
           docenteSeleccionado = parseFloat(docenteSeleccionado);
           var fechaInicio = new Date();
-          self.vinculaciondocente.push(
-            {
-              "Activo": true,
-              "FechaInicio": fechaInicio,
-              "IdTipoVinculacion": {
-                "Id": 1
-              },
-              "IdTrabajoGrado": dataIdTG[0],
-              "IdentificacionDocente": docenteSeleccionado
-            }
-          );
+          self.vinculaciondocente.push({
+            "Activo": true,
+            "FechaInicio": fechaInicio,
+            "IdTipoVinculacion": {
+              "Id": 1
+            },
+            "IdTrabajoGrado": dataIdTG[0],
+            "IdentificacionDocente": docenteSeleccionado
+          });
           return self.vinculaciondocente;
 
         };
         /**/
-        self.preguardarEstudianteTG = function (idTG, estudiante) {
+        self.preguardarEstudianteTG = function(idTG, estudiante) {
           estudiante = parseInt(estudiante);
-          self.estudiante_TG.push(
-            {
-              "CodigoEstudiante": estudiante,
-              "Estado": "activo",
-              "IdTrabajoGrado": { "Id": idTG }
-            });
+          self.estudiante_TG.push({
+            "CodigoEstudiante": estudiante,
+            "Estado": "activo",
+            "IdTrabajoGrado": {
+              "Id": idTG
+            }
+          });
           console.log(self.estudiante_TG);
           return self.estudiante_TG;
         };
 
         /**/
-        self.guardarestudianteTG = function (data) {
-          poluxRequest.post("estudiante_tg", data).then(function (response) {
+        self.guardarestudianteTG = function(data) {
+          poluxRequest.post("estudiante_tg", data).then(function(response) {
             console.log("respuesta del post estudiante_tg: " + response.data.Id);
             return response.data.Id;
           });
-        }
+        };
         /**/
-        self.guardarDocumentoTG = function (idDocumento, IdTrabajoGrado) {
-          self.docTG.push(
-            {
-              "IdDocumento": {
-                "Id": idDocumento
-              },
-              "IdEstadoDocumento": {
-                "Id": 1
-              },
-              "IdTrabajoGrado": { "Id": IdTrabajoGrado }
+        self.guardarDocumentoTG = function(idDocumento, IdTrabajoGrado) {
+          self.docTG.push({
+            "IdDocumento": {
+              "Id": idDocumento
+            },
+            "IdEstadoDocumento": {
+              "Id": 1
+            },
+            "IdTrabajoGrado": {
+              "Id": IdTrabajoGrado
             }
-          );
+          });
           self.docTGregistrado = {};
-          poluxRequest.post("documento_tg", self.docTG[0]).then(function (response) {
+          poluxRequest.post("documento_tg", self.docTG[0]).then(function(response) {
             poluxRequest.get("documento_tg", $.param({
               query: "Id:" + response.data.Id
-            })).then(function (response) {
+            })).then(function(response) {
               self.docTGregistrado = response.data;
             });
           });
         };
         /**/
-        self.asignarAreasTG = function (parametro) {
-          angular.forEach(parametro, function (valor) {
-            poluxRequest.post("areas_trabajo_grado", valor).then(function (response) {
+        self.asignarAreasTG = function(parametro) {
+          angular.forEach(parametro, function(valor) {
+            poluxRequest.post("areas_trabajo_grado", valor).then(function(response) {
               poluxRequest.get("areas_trabajo_grado", $.param({
                 query: "IdTrabajoGrado:" + response.data.IdTrabajoGrado
-              })).then(function (response) {
+              })).then(function(response) {
                 self.areasTG = response.data;
               });
             });
@@ -299,11 +296,10 @@ angular.module('poluxClienteApp')
         };
 
         /**/
-        self.limpiar = function () {
+        self.limpiar = function() {
           self.documento = angular.copy(self.doclimpio);
           self.nuevaArea = [];
         };
-
       },
       controllerAs: 'd_regProp'
     };
