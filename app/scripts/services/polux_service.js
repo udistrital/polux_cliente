7f8dc4b12d215dcb8c9c05aac62ee45aa6c56e50
Fabian D. Sanchez L <fadarsaleeing@gmail.com>
2017-07-24 10:47:31 -0500

    resolviendo errores de la transacción

--- app/scripts/services/polux_service.js
+++ app/scripts/services/polux_service.js
@@ -7,31 +7,31 @@
  * # poluxService
  * Factory in the poluxClienteApp.
  */
-angular.module('poluxService',[])
-  .factory('poluxRequest', function($http) {
-    // Service logic
-    //var path = "http://localhost:8085/v1/";
-    var path = "http://10.20.0.254/polux_api_crud/v1/";
+angular.module('poluxService', [])
+    .factory('poluxRequest', function($http) {
+        // Service logic
+        var path = "http://localhost:8080/v1/";
+        //var path = "http://10.20.0.254/polux_api_crud/v1/";
 
-    // Public API here
-    return {
-      get: function(tabla, params) {
-          var peticion = path + tabla + "?" + params;
-          return $http.get(peticion);
-      },
-      post: function(tabla, elemento) {
-        //se realiza definicion de post con formato header, para resolucion del problema post
-        return $http.post(path + tabla, elemento, {
-          headers : {
-              'Content-Type' : 'application/x-www-form-urlencoded; charset=UTF-8'
-           }
-       });
-      },
-      put: function(tabla, id, elemento) {
-        return $http.put(path + tabla + "/" + id, elemento);
-      },
-      delete: function(tabla, id) {
-        return $http.delete(path + tabla + "/" + id);
-      }
-    };
-  });
+        // Public API here
+        return {
+            get: function(tabla, params) {
+                var peticion = path + tabla + "?" + params;
+                return $http.get(peticion);
+            },
+            post: function(tabla, elemento) {
+                //se realiza definicion de post con formato header, para resolucion del problema post
+                return $http.post(path + tabla, elemento, {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+                    }
+                });
+            },
+            put: function(tabla, id, elemento) {
+                return $http.put(path + tabla + "/" + id, elemento);
+            },
+            delete: function(tabla, id) {
+                return $http.delete(path + tabla + "/" + id);
+            }
+        };
+    });
