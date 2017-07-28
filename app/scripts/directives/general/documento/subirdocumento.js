85cf3f93ad1b85554aed01e9b96719c32860ae50
fabianLeon <fadarsaleeing@gmail.com>
2017-07-28 05:41:31 -0500

    construyendo documento tipo file y subiendo archivo

--- app/scripts/directives/general/documento/subirdocumento.js
+++ app/scripts/directives/general/documento/subirdocumento.js
@@ -20,6 +20,7 @@ angular.module('poluxClienteApp')
             controller: function(nuxeo, $scope) {
                 var ctrl = this;
                 ctrl.msg = null;
+
                 nuxeo.connect().then(function(client) {
                     // OK, the returned client is connected
                     console.log('Client is connected: ' + client.connected);
@@ -28,43 +29,54 @@ angular.module('poluxClienteApp')
                     console.log('Client is not connected: ' + err);
                 });
 
-                /* $("#fileupload2").fileinput({
-                    language: 'es',
-                    uploadUrl: 'http://10.20.2.129:8080/nuxeo/api/v1/',
-                    uploadAsync: true,
-                    maxFileCount: 5,
-                    showBrowse: true,
-                    browseOnZoneClick: true,
-                    showAjaxErrorDetails: true,
-                    elErrorContainer: '#errorBlock'
-                });
-
-                $('#fileupload2').on('fileuploaderror', function(event, data, previewId, index) {
-                    var form = data.form,
-                        files = data.files,
-                        extra = data.extra,
-                        response = data.response,
-                        reader = data.reader;
-                });
-
-                $('#fileupload2').on('fileuploaded', function(event, data, previewId, index) {
-                    console.log(data);
-                    ctrl.archivo = data;
-                });*/
-                ctrl.subir_doc_2 = function() {
-
+                ctrl.construir_propuesta = function() {
+                    nuxeo.operation('Document.Create')
+                        .params({
+                            type: 'File',
+                            name: ctrl.titulo,
+                            properties: 'dc:title=' + ctrl.titulo + ' \ndc:description=' + ctrl.resumen
+                        })
+                        .input('/default-domain/workspaces/Proyectos')
+                        .execute()
+                        .then(function(doc) {
+                            console.log(doc);
+                            var nuxeoBlob = new Nuxeo.Blob({ content: $scope.fileModel });
+                            console.log(nuxeoBlob);
+                            nuxeo.batchUpload()
+                                .upload(nuxeoBlob)
+                                .then(function(res) {
+                                    return nuxeo.operation('Blob.AttachOnDocument')
+                                        .param('document', doc.uid)
+                                        .input(res.blob)
+                                        .execute();
+                                })
+                                .then(function() {
+                                    return nuxeo.repository().fetch(doc.uid, { schemas: ['dublincore', 'file'] });
+                                })
+                                .then(function(doc) {
+                                    console.log(doc.get('file:content'));
+                                })
+                                .catch(function(error) {
+                                    throw error;
+                                });
+                        })
+                        .catch(function(error) {
+                            throw error;
+                        });
+                };
+                ctrl.subir_archivo = function() {
                     var nuxeoBlob = new Nuxeo.Blob({ content: $scope.fileModel });
                     console.log(nuxeoBlob);
                     nuxeo.batchUpload()
                         .upload(nuxeoBlob)
                         .then(function(res) {
                             return nuxeo.operation('Blob.AttachOnDocument')
-                                .param('document', '33f68e5b-014c-4e7b-aa5a-0332e3f6402b')
+                                .param('document', '1dca9360-5d24-4190-8d0c-411aef25453c')
                                 .input(res.blob)
                                 .execute();
                         })
                         .then(function() {
-                            return nuxeo.repository().fetch('33f68e5b-014c-4e7b-aa5a-0332e3f6402b', { schemas: ['dublincore', 'file'] });
+                            return nuxeo.repository().fetch('1dca9360-5d24-4190-8d0c-411aef25453c', { schemas: ['dublincore', 'file'] });
                         })
                         .then(function(doc) {
                             console.log(doc.get('file:content'));
@@ -72,58 +84,7 @@ angular.module('poluxClienteApp')
                         .catch(function(error) {
                             throw error;
                         });
-
-                    $q.when(nuxeo.request('/http://10.20.2.129:8080/nuxeo/api/v1/upload/').get()).then(function(res) {
-                        $scope.res = res;
-                    });
-                    /*nuxeo.operation('Document.Create')
-                        .params({
-                            username: 'Administrator',
-                            type: 'File',
-                            name: $scope.fileModel.name,
-                            File: $scope.fileModel,
-                            properties: 'dc:title=' + $scope.fileModel.name + '\ndc:description=A Simple Folder'
-                        })
-                        .input('/')
-                        .execute()
-                        .then(function(doc) {
-                            console.log('Created ' + doc.title + ' folder');
-                        })
-                        .catch(function(error) {
-                            throw error;
-                        });*/
                 };
-                ctrl.subir_doc = function() {
-                    console.log($scope.fileModel);
-                    var nuxeoBlob = new Nuxeo.Blob({ content: $scope.fileModel });
-                    console.log(nuxeoBlob);
-                    nuxeo.batchUpload()
-                        .upload(nuxeoBlob)
-                        .then(function(res) {
-                            // res.blob instanceof Nuxeo.BatchBlob
-                            console.log(res.blob);
-                        })
-                        .catch(function(error) {
-                            throw error;
-                        });
-                    /*nuxeo.operation('Document.Create')
-                        .params({
-                            username: 'Administrator',
-                            type: 'File',
-                            name: $scope.fileModel.name,
-                            File: $scope.fileModel,
-                            properties: 'dc:title=' + $scope.fileModel.name + '\ndc:description=A Simple Folder'
-                        })
-                        .input('/')
-                        .execute()
-                        .then(function(doc) {
-                            console.log('Created ' + doc.title + ' folder');
-                        })
-                        .catch(function(error) {
-                            throw error;
-                        });*/
-                };
-
             },
             controllerAs: 'd_subirDocumento'
         };
