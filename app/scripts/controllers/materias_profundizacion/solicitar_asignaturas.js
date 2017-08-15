f2e59e163649f1e629d5d7e88c0b28be90fa1293
Mafe Avendaño <mafe.ud@gmail.com>
2017-08-15 06:42:19 -0500

    Verificación de requisitos en la modalidad de espacios académicos de profundización

--- app/scripts/controllers/materias_profundizacion/solicitar_asignaturas.js
+++ app/scripts/controllers/materias_profundizacion/solicitar_asignaturas.js
@@ -8,13 +8,59 @@
  * Controller of the poluxClienteApp
  */
 angular.module('poluxClienteApp')
-  .controller('MateriasProfundizacionSolicitarAsignaturasCtrl', function () {
+  .controller('MateriasProfundizacionSolicitarAsignaturasCtrl', function (poluxMidRequest, academicaRequest, $scope) {
     var ctrl = this;
+    $scope.codigo="20092020008";
 
-    ctrl.estudiante={
-      "Codigo": "20102020008",
-      "Nombre": "María Fernanda Avendaño",
-      "Tipo": "PREGRADO"
-    };
-    ctrl.modalidad="MATERIAS PROFUNDIZACION";
+    $scope.$watch("codigo",function() {
+
+      academicaRequest.periodoAnterior().then(function(periodoAnterior){
+
+        var parametros = {
+          "codigo": $scope.codigo,
+          //periodo anterior
+          'ano' : periodoAnterior[0].APE_ANO,
+          'periodo' :periodoAnterior[0].APE_PER
+        };
+
+        academicaRequest.promedioEstudiante(parametros).then(function(response2){
+
+          if(response2){
+            //porcentaje cursado
+            var parametros2 = {
+              "codigo": parametros.codigo
+            };
+
+            academicaRequest.porcentajeCursado(parametros).then(function(response3){
+              console.log(response3);
+
+              ctrl.estudiante={
+                "Codigo": parametros.codigo,
+                "Nombre": response2[0].NOMBRE,
+                "Modalidad": 7,
+                "Tipo": "PREGRADO",
+                "PorcentajeCursado": response3,
+                "Promedio": response2[0].PROMEDIO,
+                "Rendimiento": "0"+response2[0].REG_RENDIMIENTO_AC,
+                "Estado": response2[0].EST_ESTADO_EST,
+                "Nivel": response2[0].TRA_NIVEL,
+                //"TipoCarrera": response2[0].TRA_NOMBRE
+                "TipoCarrera": "TECNOLOGIA"
+
+              };
+
+              console.log(ctrl.estudiante);
+              ctrl.modalidad="MATERIAS PROFUNDIZACION";
+
+              poluxMidRequest.post("verificarRequisitos/Registrar", ctrl.estudiante).then(function(response){
+                  console.log(response);
+                  ctrl.validar= response.data;
+              });
+            });
+          }
+
+        });
+
+      });
+    });
   });
