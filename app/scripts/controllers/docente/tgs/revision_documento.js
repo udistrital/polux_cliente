788d1cd8f2110efaa8442ec1910e6b064bd99e19
Mafe Avendaño <mafe.ud@gmail.com>
2017-10-17 09:58:19 -0500

    #16978# Ajustes revisión de documentos desde el docente

--- app/scripts/controllers/docente/tgs/revision_documento.js
+++ app/scripts/controllers/docente/tgs/revision_documento.js
@@ -20,41 +20,39 @@ angular.module('poluxClienteApp')
 
         ctrl.ver_seleccion = function(item, model) {
             console.log(item);
-            ctrl.tgId = item.IdTrabajoGrado.Id;
+            ctrl.tgId = item.TrabajoGrado.Id;
             ctrl.doctgId = item.DocumentoTrabajoGrado[0].Id;
-            ctrl.doc = item.DocumentoTrabajoGrado[0].IdDocumento.Id;
+            ctrl.doc = item.DocumentoTrabajoGrado[0].Documento.Id;
             ctrl.documento = true;
             ctrl.vncdocId = item.Id;
             ctrl.refrescar();
         };
 
-        poluxRequest.get("vinculacion_docente", $.param({
+        poluxRequest.get("vinculacion_trabajo_grado", $.param({
             limit: -1,
             sortby: "Id",
             order: "asc"
         })).then(function(response) {
             ctrl.vinculacion_docente = response.data;
             angular.forEach(ctrl.vinculacion_docente, function(vd) {
-                poluxRequest.get("documento_tg", $.param({
+                poluxRequest.get("documento_trabajo_grado", $.param({
                     limit: -1,
                     sortby: "Id",
                     order: "asc",
-                    query: "IdTrabajoGrado:" + vd.IdTrabajoGrado.Id
+                    query: "TrabajoGrado:" + vd.TrabajoGrado.Id
                 })).then(function(response) {
                     vd.DocumentoTrabajoGrado = response.data;
                 });
                 $http.get("http://10.20.0.127/polux/index.php?data=sj7574MlJOsg4LjjeAOJP5CBi1dRh84M-gX_Z-i_0OmWhton7vEvfcvwRdSGHCTl2WlcEunFl-15PLUWhzSwdnO0c9_4iv7A6ODAQz8nzk3-L-wp9KXARJdYvqggsPUb&identificacion=" + vd.IdentificacionDocente)
                     .then(function(response) {
-                        var json = response.data.split("<json>");
-                        var jsonObj = JSON.parse(json[1]);
-                        vd.Docente = jsonObj[0];
+                        vd.Docente = response.data[0];
                     });
             });
         });
         ctrl.refrescar = function() {
-            poluxRequest.get("revision", $.param({
+            poluxRequest.get("revision_trabajo_grado", $.param({
                 limit: -1,
-                query: "IdDocumentoTg:" + ctrl.doctgId + ",IdVinculacionDocente:" + ctrl.vncdocId,
+                query: "DocumentoTrabajoGrado:" + ctrl.doctgId + ",VinculacionTrabajoGrado:" + ctrl.vncdocId,
                 sortby: "Id",
                 order: "asc"
             })).then(function(response) {
