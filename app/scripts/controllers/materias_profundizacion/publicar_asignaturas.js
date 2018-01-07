b2e97bfe09544746f201f6e2d20cf01a3dcf3225
Mafe Avendaño <mafe.ud@gmail.com>
2018-01-07 00:58:21 -0500

    Se listan las carreras de acuerdo al usuario, para la publicación de espacios académicos en la modalidad de materias de profundización. Se cambian selects por ui-select.

--- app/scripts/controllers/materias_profundizacion/publicar_asignaturas.js
+++ app/scripts/controllers/materias_profundizacion/publicar_asignaturas.js
@@ -8,8 +8,9 @@
  * Controller of the poluxClienteApp
  */
 angular.module('poluxClienteApp')
-  .controller('MateriasProfundizacionPublicarAsignaturasCtrl', function (academicaRequest) {
+  .controller('MateriasProfundizacionPublicarAsignaturasCtrl', function (academicaRequest, $scope) {
     var ctrl = this;
+    $scope.userId = "60261576";
     ctrl.periodo=[];
     ctrl.carreras=[];
     ctrl.modalidad="PREGRADO";
@@ -18,11 +19,30 @@ angular.module('poluxClienteApp')
       ctrl.periodo=response[0];
     });
 
-    academicaRequest.obtenerCarreras({
+    $scope.$watch("userId",function() {
+        var parametrosCoordinador = {
+          'identificacion':$scope.userId,
+          'tipo': 'PREGRADO'
+        };
+        //ctrl.conSolicitudes = false;
+        academicaRequest.obtenerCoordinador(parametrosCoordinador).then(function(responseCoordinador){
+              ctrl.carreras = [];
+              if(responseCoordinador!=="null"){
+                  ctrl.carreras = responseCoordinador;
+                  angular.forEach(responseCoordinador, function(carrera){
+                      carreras.push(carrera.CODIGO_CARRERA);
+                  });
+              }
+        });
+
+      $scope.load = true;
+    });
+
+    /*academicaRequest.obtenerCarreras({
       'tipo': 'PREGRADO'
     }).then(function(response){
       ctrl.carreras=response;
-    });
+    });*/
 
     ctrl.myFunc = function(carreraSeleccionada) {
       ctrl.pensums=[];
