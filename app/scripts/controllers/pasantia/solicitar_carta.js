09bdde0b6b0ba022a88a1f3199eff0d450e35b12
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-02-12 15:58:36 -0500

    Post de la solicitud

--- app/scripts/controllers/pasantia/solicitar_carta.js
+++ app/scripts/controllers/pasantia/solicitar_carta.js
@@ -8,7 +8,7 @@
  * Controller of the poluxClienteApp
  */
 angular.module('poluxClienteApp')
-  .controller('PasantiaSolicitarCartaCtrl', function ($scope,$translate,academicaRequest,poluxRequest,poluxMidRequest) {
+  .controller('PasantiaSolicitarCartaCtrl', function ($location,$q,$scope,$translate,academicaRequest,poluxRequest,poluxMidRequest) {
     var ctrl = this;
 
     ctrl.validarRequisitosEstudiante = function(){
@@ -57,6 +57,94 @@ angular.module('poluxClienteApp')
     $scope.loadEstudiante = true;
     ctrl.validarRequisitosEstudiante();
 
+    ctrl.postSolicitud = function(){
+      var defer = $q.defer();
+
+      var data_detalles = [];
+      var data_solicitante = [];
+      var data_respuesta = {};
+      var fecha = new Date();
+
+      //datos de la solicitud
+      var data_solicitud={
+        "Fecha": fecha,
+        "ModalidadTipoSolicitud": {
+          //id solicitud de carta en modalidad_tipo_solicitud
+          "Id": 1
+        }
+      };
+
+      //detalles de la solicitud, nombre empresa, nombre encargado y cargo
+      data_detalles.push({
+         "Descripcion": ctrl.nombreEmpresa,
+         "SolicitudTrabajoGrado": {
+           "Id": 0
+         },
+         "DetalleTipoSolicitud": {
+           "Id": 1
+         }
+      });
+      data_detalles.push({
+         "Descripcion": ctrl.nombreReceptor,
+         "SolicitudTrabajoGrado": {
+           "Id": 0
+         },
+         "DetalleTipoSolicitud": {
+           "Id": 2
+         }
+      });
+      data_detalles.push({
+         "Descripcion": ctrl.cargoReceptor,
+         "SolicitudTrabajoGrado": {
+           "Id": 0
+         },
+         "DetalleTipoSolicitud": {
+           "Id": 3
+         }
+      });
+
+      //informacion del solicitante
+      data_solicitante.push({
+        "Usuario":ctrl.codigo,
+        "SolicitudTrabajoGrado": {
+          "Id": 0
+        }
+      });
+
+      //Respuesta de la solicitud
+       data_respuesta={
+         "Fecha": fecha,
+         "Justificacion": "La solicitud fue radicada",
+         "EnteResponsable":0,
+         "Usuario": 0,
+         "EstadoSolicitud": {
+           "Id": 1
+         },
+         "SolicitudTrabajoGrado": {
+           "Id": 0
+         },
+         "Activo": true
+       }
+
+       //se crea objeto con las solicitudes
+       ctrl.postSolicitud={
+         Solicitud: data_solicitud,
+         Respuesta: data_respuesta,
+         DetallesSolicitud: data_detalles,
+         UsuariosSolicitud: data_solicitante
+       }
+
+       poluxRequest.post("tr_solicitud", ctrl.postSolicitud)
+       .then(function(response) {
+          defer.resolve(response);
+       })
+       .catch(function(error){
+          defer.reject(error);
+       });
+
+      return defer.promise;
+    }
+
     ctrl.enviarSolicitud = function(){
       //alert(ctrl.nombreEmpresa);
 
@@ -69,14 +157,34 @@ angular.module('poluxClienteApp')
                cancelButtonText: $translate.instant("CANCELAR"),
                showCancelButton: true
       }).then(function(){
-        swal({
-          title: $translate.instant("INFORMACION_SOLICITUD"),
-          text: $translate.instant("PASANTIA.RECLAMAR_CARTA"),
-          type: "warning",
-          confirmButtonText: $translate.instant("ACEPTAR"),
-          cancelButtonText: $translate.instant("CANCELAR"),
-          showCancelButton: true
-        });
+          $scope.loadFormulario = false;
+          ctrl.postSolicitud()
+          .then(function(response){
+            console.log(response.data);
+             if(response.data[0]==="Success"){
+               swal(
+                 $translate.instant("FORMULARIO_SOLICITUD"),
+                 $translate.instant("SOLICITUD_REGISTRADA"),
+                 'success'
+               );
+               $location.path("/solicitudes/listar_solicitudes");
+             }else{
+               swal(
+                 $translate.instant("FORMULARIO_SOLICITUD"),
+                 $translate.instant(response.data[1]),
+                 'warning'
+               );
+             }
+             $scope.loadFormulario = false;
+          })
+          .catch(function(error){
+            swal(
+              $translate.instant("FORMULARIO_SOLICITUD"),
+              $translate.instant("ERROR_CARGAR_SOLICITUDES"),
+              'warning'
+            );
+          })
+
       });
     }
 
