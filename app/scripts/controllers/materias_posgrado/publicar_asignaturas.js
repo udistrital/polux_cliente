338ae15f5d6f0f18e084329cc8ff1ede373c20a3
juanpiedrahita <juangonzalez1597@gmail.com>
2018-05-02 16:29:35 -0500

    #22485# errores al publicar asignaturas

--- app/scripts/controllers/materias_posgrado/publicar_asignaturas.js
+++ app/scripts/controllers/materias_posgrado/publicar_asignaturas.js
@@ -15,11 +15,6 @@ angular.module('poluxClienteApp')
     ctrl.periodo=[];
     ctrl.modalidad="POSGRADO";
 
-    academicaRequest.get("periodo_academico","X").then(function(response){
-        if (!angular.isUndefined(response.data.periodoAcademicoCollection.periodoAcademico)) {
-            ctrl.periodo=response.data.periodoAcademicoCollection.periodoAcademico[0];
-        }
-    });
 
     $scope.$watch("userId",function() {
         $scope.msgCargandoSolicitudes = $translate.instant('LOADING.CARGANDO_ASIGNATURAS');
@@ -28,17 +23,40 @@ angular.module('poluxClienteApp')
         academicaRequest.get("coordinador_carrera", [$scope.userId, "POSGRADO"]).then(function(response){
           console.log(response);
         	if (!angular.isUndefined(response.data.coordinadorCollection.coordinador)) {
-            	ctrl.carreras=response.data.coordinadorCollection.coordinador;
-        	}
-    	  });
-
-        $scope.load = false;
+                ctrl.carreras=response.data.coordinadorCollection.coordinador;
+                academicaRequest.get("periodo_academico","X").then(function(response){
+                    if (!angular.isUndefined(response.data.periodoAcademicoCollection.periodoAcademico)) {
+                        ctrl.periodo=response.data.periodoAcademicoCollection.periodoAcademico[0];
+                    }else{
+                        ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGANDO_PERIODO');
+                        ctrl.errorCargarParametros = true;
+                    }
+                    $scope.load = false;
+                })
+                .catch(function(error){
+                    console.log(error);
+                    ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGANDO_PERIODO');
+                    ctrl.errorCargarParametros = true;
+                    $scope.load = false;
+                });
+        	} else{
+                ctrl.mensajeErrorCarga = $translate.instant('NO_CARRERAS_POSGRADO');
+                ctrl.errorCargarParametros = true;
+                $scope.load = false;
+            }
+          })
+          .catch(function(error){
+              console.log(error);
+              ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGAR_CARRERAS');
+              ctrl.errorCargarParametros = true;
+              $scope.load = false;
+          });
     });
 
     ctrl.myFunc = function(carreraSeleccionada) {
+      $scope.load = true;
       $scope.pensumSeleccionado=null;
       $scope.msgCargandoPensums = $translate.instant('LOADING.CARGANDO_PENSUMS');
-      $scope.load = true;
       ctrl.pensums=[];
       academicaRequest.get("pensums",[carreraSeleccionada]).then(function(response){
           if (!angular.isUndefined(response.data.pensums.pensum)) {
@@ -46,8 +64,18 @@ angular.module('poluxClienteApp')
               ctrl.pensums=response.data.pensums.pensum;
               ctrl.pensumSeleccionado=null;
               $scope.load = false;
-          }
-      });
+          } else{
+            ctrl.mensajeCargaPensum = $translate.instant('NO_PENSUMS');
+            ctrl.errorCargarPensum = true;
+            $scope.load = false;
+            }
+      })
+      .catch(function(error){
+        console.log(error);
+        ctrl.mensajeCargaPensum = $translate.instant('ERROR.CARGAR_CARRERAS');
+        ctrl.errorCargarPensum = true;
+        $scope.load = false;
+    });
     };
 
   });
