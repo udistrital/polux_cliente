2341c718c69d70d060e31d1f468340ebc961af5e
juanpiedrahita <juangonzalez1597@gmail.com>
2018-05-10 12:00:24 -0500

    #22634# Se documenta directiva loading

--- app/scripts/directives/loading.js
+++ app/scripts/directives/loading.js
@@ -4,7 +4,12 @@
  * @ngdoc directive
  * @name poluxClienteApp.directive:loading
  * @description
- * # loading: Directiva que se muestra mientras se carga una petición
+ * # loading
+ * Directiva que se muestra mientras se carga una petición
+ * Controlador: {@link poluxClienteApp.directive:loading.controller:loadingCtl loadingCtl}
+ * @param {boolean} load Flag que permite identificar cuando deja de mostrarse la animación
+ * @param {number} tam Tamaño de la imagen de cargando
+ * @param {string} message Mensaje que se muestra bajo la animación
  */
 angular.module('poluxClienteApp')
 .directive('loading', function () {
@@ -23,7 +28,23 @@ angular.module('poluxClienteApp')
                     '<label class="col-sm-12 control-label" style="text-align: center;"> {{loadMessage}}</label>'+
                 '</div>'
                 ,
+      /**
+       * @ngdoc controller
+       * @name poluxClienteApp.directive:loading.controller:loadingCtl
+       * @description
+       * # MateriasPosgradoFormalizarSolicitudCtrl
+       * Controller of the poluxClienteApp.directive:loading
+       * Controlador de la directiva loading
+       * @requires $scope
+       */
       controller:function($scope){
+        /**
+         * @ngdoc method
+         * @name mostrar
+         * @methodOf poluxClienteApp.directive:loading.controller:loadingCtl
+         * @description 
+         * Muestra o oculta la animación con la bandera load
+         */  
         $scope.loadMessage = "";
         if ($scope.tam===undefined) {
           $scope.tam=5;
