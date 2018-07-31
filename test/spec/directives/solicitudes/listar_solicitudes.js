d75ec07dbe2d36fa1befb1f9ec7f8ba43387044d
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-07-31 14:03:47 -0500

    #24260# Directiva para ver las solicitudes asociadas a un trabajo de grado, se asocia la directiva al modulo de consulta de trabajo de grado

---
+++ test/spec/directives/solicitudes/listar_solicitudes.js
@@ -0,0 +1,20 @@
+'use strict';
+
+describe('Directive: solicitudes/listarSolicitudes', function () {
+
+  // load the directive's module
+  beforeEach(module('poluxClienteApp'));
+
+  var element,
+    scope;
+
+  beforeEach(inject(function ($rootScope) {
+    scope = $rootScope.$new();
+  }));
+
+  it('should make hidden element visible', inject(function ($compile) {
+    element = angular.element('<solicitudes/listar-solicitudes></solicitudes/listar-solicitudes>');
+    element = $compile(element)(scope);
+    expect(element.text()).toBe('this is the solicitudes/listarSolicitudes directive');
+  }));
+});
