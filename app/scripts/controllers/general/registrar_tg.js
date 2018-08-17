34f2def4eefeaf98f719dc6ca43afd203a44951c
karianov <karianov@correo.udistrital.edu.co>
2018-08-17 17:29:35 -0500

    #25179# Documentación del controlador 'general/registrar_tg'

--- app/scripts/controllers/general/registrar_tg.js
+++ app/scripts/controllers/general/registrar_tg.js
@@ -1,17 +1,20 @@
 'use strict';
 
 /**
- * @ngdoc function
+ * @ngdoc controller
  * @name poluxClienteApp.controller:RegistrarTgCtrl
  * @description
- * # RegistrarTgCtrl
+ * # GeneralRegistrarNotaCtrl
  * Controller of the poluxClienteApp
+ * Actualmente no se utiliza este controlador
+ * @requires services/academicaService.service:academicaRequest
  */
 angular.module('poluxClienteApp')
-  .controller('RegistrarTgCtrl', function (academicaRequest) {
+	.controller('RegistrarTgCtrl',
+		function(academicaRequest) {
 
-    var self=this;
+			var self = this;
 
-    self.estudiantes=academicaRequest.getAllEstudiantesJson(); //cambiar a self.estudiantes=academicaRequest.ObtenerEstudiantes();
+			self.estudiantes = academicaRequest.getAllEstudiantesJson(); //cambiar a self.estudiantes=academicaRequest.ObtenerEstudiantes();
 
-  });
+		});
