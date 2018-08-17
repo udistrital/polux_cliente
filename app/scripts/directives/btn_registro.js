9d7361299b00e0f027a79353557d26bc248aee7f
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-08-17 11:18:39 -0500

    #25179# Comentarios de la directiva btn_registro

--- app/scripts/directives/btn_registro.js
+++ app/scripts/directives/btn_registro.js
@@ -5,7 +5,8 @@
  * @name poluxClienteApp.directive:btnRegistro
  * @description
  * # btnRegistro
- * Directiva que muestra los botones en el ui-grid
+ * Directiva que muestra los botones en el ui-grid, esta directiva no tiene controlador, se manejan las funcioens y eventos 
+ * por medio del $scope.
  * @param {object} fila Fila del ui-grid donde se enccuentran los botones
  * @param {function} funcion función que se ejecuta cuando se selecciona alguno de los botones
  * @param {object} grupobotones Botones que se muestra
