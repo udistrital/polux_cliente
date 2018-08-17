1d4f07a133c03ae0d56a568f955884a4a34372ea
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-08-17 16:58:36 -0500

    #25179# Comentarios de la directiva llenar_evaluacion

--- app/scripts/directives/formato/llenar_evaluacion.js
+++ app/scripts/directives/formato/llenar_evaluacion.js
@@ -2,11 +2,26 @@
 
 /**
  * @ngdoc directive
- * @name poluxClienteApp.directive:formato/llenarEvaluacion
+ * @name poluxClienteApp.directive:llenarEvaluacion
  * @description
- * # formato/llenarEvaluacion
+ * # llenarEvaluacion
+ * Directiva que permite llenar una evaluación de un trabajo de grado.
+ * Controlador: {@link poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl llenarEvaluacionCtrl}
+ * @param {object} formato Formato que se va a llenar para evaluar el trabajo de grado.
  */
 angular.module('poluxClienteApp')
+    /**
+       * @ngdoc controller
+       * @name poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl
+       * @description
+       * # llenarEvaluacionCtrl
+       * # Controller of the poluxClienteApp.directive:llenarEvaluacion
+       * Controlador de la directiva {@link poluxClienteApp.directive:llenarEvaluacion llenarEvaluacion}.
+       * @requires services/poluxService.service:poluxRequest
+       * @requires $scope
+       * @property {array} enviar Respuestas que da el usuario al formato y que se enviarán para registrar.
+       * @property {object} formato Formato que se llenará, contiene los campos que deben ser llenados.
+       */
     .directive('llenarEvaluacion', function() {
         return {
             restrict: 'E',
@@ -20,6 +35,15 @@ angular.module('poluxClienteApp')
                 $scope.selected = [];
                 ctrl.enviar = [];
 
+                /**
+                 * @ngdoc method
+                 * @name enviar_evaluacion
+                 * @methodOf poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl
+                 * @param {undefined} undefined No recibe parametros
+                 * @returns {undefined} No retorna ningún valor.
+                 * @description 
+                 * Permite añadir las respuestas dadas por el usuario al arreglo de respuestas que se envian.
+                 */
                 ctrl.enviar_evaluacion = function() {
                     angular.forEach($scope.selected, function(data) {
                         ctrl.enviar.push({
@@ -28,10 +52,15 @@ angular.module('poluxClienteApp')
                     });
                 };
 
-                $scope.$watch('formato', function() {
-                    console.log(ctrl.Formato);
-                });
-
+                /**
+                 * @ngdoc method
+                 * @name cargarFormato
+                 * @methodOf poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl
+                 * @param {undefined} undefined No recibe parametros
+                 * @returns {undefined} No retorna ningún valor.
+                 * @description 
+                 * Permite cargar el formato y la data asociada a este.
+                 */
                 poluxRequest.get("tr_formato/" + $scope.formato, "")
                     .then(function(response) {
                         ctrl.Formato = response.data;
@@ -42,6 +71,17 @@ angular.module('poluxClienteApp')
                         });
                     });
 
+                
+                /**
+                 * @ngdoc method
+                 * @name toggle
+                 * @methodOf poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl
+                 * @param {object} item Item que se va a agragar o eliminar
+                 * @param {array} list Lista de elementos de la que se elimina o agrega el objeto.
+                 * @returns {undefined} No retorna ningún valor.
+                 * @description 
+                 * Permite agregar o eliminar objetos de una lista.
+                 */
                 $scope.toggle = function(item, list) {
                     var idx = list.indexOf(item);
                     if (idx > -1) {
@@ -51,6 +91,16 @@ angular.module('poluxClienteApp')
                     }
                 };
 
+                /**
+                 * @ngdoc method
+                 * @name exists
+                 * @methodOf poluxClienteApp.directive:llenarEvaluacion.controller:llenarEvaluacionCtrl
+                 * @param {object} item Item que se busca
+                 * @param {array} list Lista de elementos en la que se debe realizar la busqueda
+                 * @returns {boolean} Booleano que indica si el elemento se encontro o no.
+                 * @description 
+                 * Permite buscar objetos en una lista.
+                 */
                 $scope.exists = function(item, list) {
                     return list.indexOf(item) > -1;
                 };
