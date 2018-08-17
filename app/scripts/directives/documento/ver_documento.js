441b4b5d0fc9947e871d816e57290eda68e56025
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-08-17 16:09:05 -0500

    #25179# Comentarios de la directiva ver_documento

--- app/scripts/directives/documento/ver_documento.js
+++ app/scripts/directives/documento/ver_documento.js
@@ -5,9 +5,13 @@
  * @name poluxClienteApp.directive:verDocumento
  * @description
  * # verDocumento
+ * Directiva que permite cargar un documento a nuxeo.
+ * Controlador: {@link poluxClienteApp.directive:verDocumento.controller:verDocumentoCtrl verDocumentoCtrl}
+ * @param {boolean} minified Booleano que indica si el documento esta minimizado.
+ * @param {object} documento Documento que se va a mostrar
  */
 angular.module('poluxClienteApp')
-    .directive('verDocumento', function(poluxRequest, constantes, nuxeoClient, $q, $sce, $translate) {
+    .directive('verDocumento', function (poluxRequest, constantes, nuxeoClient, $q, $sce, $translate) {
         return {
             restrict: "E",
             scope: {
@@ -15,32 +19,65 @@ angular.module('poluxClienteApp')
                 minified: '=?',
             },
             templateUrl: "views/directives/documento/ver_documento.html",
-            controller: function($scope) {
+            /**
+             * @ngdoc controller
+             * @name poluxClienteApp.directive:verDocumento.controller:verDocumentoCtrl
+             * @description
+             * # verDocumentoCtrl
+             * # Controller of the poluxClienteApp.directive:verDocumento
+             * Controlador de la directiva {@link poluxClienteApp.directive:verDocumento verDocumento}.
+             * @requires decorators/poluxClienteApp.decorator:TextTranslate
+             * @requires $scope
+             * @requires services/poluxClienteApp.service:nuxeoClient
+             * @property {string} msgCargandoDocumento Mensaje que se muestra mientras se esta cargando el documento.
+             * @property {string} mensajeError Error que se muestra cuando ocurre un error cargando el documento.
+             * @property {boolean} loadDocumento Load que indica que se esta cargando el documento.
+             * @property {boolean} errorDocumento Error que indica que ocuerrio un error mientras se esta cargando el documento.
+             * @property {object} pdfUrl Pdf del documento que se muestra en la vista.
+             */
+            controller: function ($scope) {
                 var self = this;
-                $scope.msgCargandoDocumento=$translate.instant('LOADING.CARGANDO_DOCUMENTO');
+                $scope.msgCargandoDocumento = $translate.instant('LOADING.CARGANDO_DOCUMENTO');
 
-                $scope.$watch('documento', function() {
-                    if($scope.documento != undefined){
-                      console.log("documento", $scope.documento);
-                      self.getDocumento($scope.documento.Enlace);
-                      self.documento = $scope.documento;
+                /**
+                 * @ngdoc watch
+                 * @name verDocumento
+                 * @methodOf poluxClienteApp.directive:verDocumento.controller:verDocumentoCtrl
+                 * @param {undefined} undefined No recibe parametros
+                 * @returns {undefined} No retorna ningún valor.
+                 * @description 
+                 * vigila cambios en documento y llama la función que carga los documentos.
+                 */
+                $scope.$watch('documento', function () {
+                    if ($scope.documento != undefined) {
+                        console.log("documento", $scope.documento);
+                        self.getDocumento($scope.documento.Enlace);
+                        self.documento = $scope.documento;
                     }
                 });
 
-
-                self.getDocumento = function(docid){
-                    $scope.loadDocumento=true;
+                /**
+                 * @ngdoc method
+                 * @name verDocumento
+                 * @methodOf poluxClienteApp.directive:verDocumento.controller:verDocumentoCtrl
+                 * @param {number} docid Uid del documento que se va a traer de nuxeo.
+                 * @returns {undefined} No retorna ningún valor.
+                 * @description 
+                 * Permite cargar un documento de {@link services/poluxClienteApp.service:nuxeoClient nuxeoClient} para mostrarlo.
+                 */
+                self.getDocumento = function (docid) {
+                    $scope.loadDocumento = true;
                     console.log(docid);
-                    if(docid!=null){
+                    if (docid != null) {
                         nuxeoClient.getDocument(docid)
-                            .then(function(documento){
-                               //$window.open(fileURL);
-                               $scope.pdfUrl=documento.url;
-                               $scope.loadDocumento=false;
+                            .then(function (documento) {
+                                //$window.open(fileURL);
+                                $scope.pdfUrl = documento.url;
+                                $scope.loadDocumento = false;
                             })
-                            .catch(function(error){
+                            .catch(function (error) {
                                 $scope.errorDocumento = true;
-                                $scope.mensajeError = $translate.instant("ERROR.CARGAR_DOCUMENTO"); 
+                                $scope.mensajeError = $translate.instant("ERROR.CARGAR_DOCUMENTO");
                                 $scope.loadDocumento = false;
                             })
                     }
