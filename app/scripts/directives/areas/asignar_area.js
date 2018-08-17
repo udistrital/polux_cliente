0db9219d5ca151f5edc125f6025b5470088793ae
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-08-17 11:40:27 -0500

    #25179# Comentarios de la directiva asignar_area

--- app/scripts/directives/areas/asignar_area.js
+++ app/scripts/directives/areas/asignar_area.js
@@ -2,9 +2,14 @@
 
 /**
  * @ngdoc directive
- * @name poluxClienteApp.directive:asignarAreas
+ * @name poluxClienteApp.directive:asignarArea
  * @description
  * # asignarArea
+ * Directiva que sirve para asignar áreas a un trabajo de grado
+ * Controlador: {@link poluxClienteApp.directive:asignarArea.controller:asignarAreaCtrl asignarAreaCtrl}
+ * @param {string} setareas Arreglo de áreas existentes
+ * @param {object} idarea Id del área que se asignará
+ * @param {number} newarea Área que se va a asignar
  */
 angular.module('poluxClienteApp')
   .directive('asignarArea', function (poluxRequest) {
@@ -12,54 +17,82 @@ angular.module('poluxClienteApp')
       restrict: 'E',
       scope: {
         setareas: '=',
-        idarea:'=',
-        newarea:'='
-        },
+        idarea: '=',
+        newarea: '='
+      },
       templateUrl: 'views/directives/areas/asignar_area.html',
-      controller:function($scope){
+      /**
+       * @ngdoc controller
+       * @name poluxClienteApp.directive:asignarArea.controller:asignarAreaCtrl
+       * @description
+       * # AsignarAreaCtrl
+       * # Controller of the poluxClienteApp.directive:asignarArea
+       * Controlador de la directiva asignar_area.
+       * @requires $scope
+       * @property {object} nuevaArea Área que se va a agregar
+       * @property {String} Nombre Nombre del área que se agrega
+       */
+      controller: function ($scope) {
         var self = this;
-        self.removable=false;
+        self.removable = false;
         self.nuevaArea = [];
 
         //Watch necesario luego de confirmar algun registro para el api
-        $scope.$watch('newarea',function(){
-          if ($scope.newarea=="") {
-            self.nuevaArea=[];
+        $scope.$watch('newarea', function () {
+          if ($scope.newarea == "") {
+            self.nuevaArea = [];
           }
         });
-        /**/
-        self.asignarArea=function(){
+        /**
+         * @ngdoc method
+         * @name asignarArea
+         * @methodOf poluxClienteApp.directive:asignarArea.controller:asignarAreaCtrl
+         * @param {undefined} undefined No recibe parametros
+         * @returns {undefined} No retorna ningún valor
+         * @description 
+         * Permite guardar el área en el arreglo de áreas del $scope.
+         */
+        self.asignarArea = function () {
           //self.Nombre=cadenaRequest.cambiarTipoTitulo(self.Nombre);
           //verifica que el Nombre sea el mismo y asigna el Id(necesario para tablas con llave compuesta)
-          angular.forEach($scope.setareas,function(value){
-            if (value.Nombre+" - "+value.Snies==self.Nombre) {
-              self.Id=value.Id;
-                if(self.nuevaArea.indexOf(value) === -1){
-                  self.nuevaArea.push(
+          angular.forEach($scope.setareas, function (value) {
+            if (value.Nombre + " - " + value.Snies == self.Nombre) {
+              self.Id = value.Id;
+              if (self.nuevaArea.indexOf(value) === -1) {
+                self.nuevaArea.push(
                   /*  { "Id":self.Id,
                     "Nombre": self.Nombre
                   }*/
-                     value
-                  );
-                }
-                self.Nombre = "";
+                  value
+                );
+              }
+              self.Nombre = "";
             }
 
           });
 
-/*        document.getElementById("formAsignar").reset();*/
-         $scope.newarea=self.nuevaArea;
+          /*        document.getElementById("formAsignar").reset();*/
+          $scope.newarea = self.nuevaArea;
         };
-        /**/
-        self.verificarAreas=function(nombreArea){
+        
+        /**
+         * @ngdoc method
+         * @name verificarAreas
+         * @methodOf poluxClienteApp.directive:asignarArea.controller:asignarAreaCtrl
+         * @param {String} nombreArea No recibe parametros
+         * @returns {Boolean} Booleano que identifica si el nombre del área que se agrega no existe ya.
+         * @description 
+         * Permite verificar que el nombre de un área es valido.
+         */
+        self.verificarAreas = function (nombreArea) {
           //nombreArea=cadenaRequest.cambiarTipoTitulo(nombreArea);
-          if ($scope.setareas==null) {
+          if ($scope.setareas == null) {
             return true;
-          }else {
-            var result=true;
+          } else {
+            var result = true;
             for (var i = 0; i < $scope.setareas.length; i++) {
-              if ($scope.setareas[i].Nombre+" - "+$scope.setareas[i].Snies==nombreArea) {
-                result= false;
+              if ($scope.setareas[i].Nombre + " - " + $scope.setareas[i].Snies == nombreArea) {
+                result = false;
               }
             }
             return result;
@@ -72,6 +105,6 @@ angular.module('poluxClienteApp')
           }*/
         };
       },
-      controllerAs:'d_asignarArea'
+      controllerAs: 'd_asignarArea'
     };
   });
