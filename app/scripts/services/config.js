7fa51cb33f68ef05f9e159de442c3977498a181f
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-10-12 10:33:02 -0500

    Se agrega variable para configuración en preprod

--- app/scripts/services/config.js
+++ app/scripts/services/config.js
@@ -76,6 +76,38 @@ var conf_local = {
 };
 
 /**
+ * @ngdoc property
+ * @name poluxClienteApp.service:CONF.conf_local
+ * @propertyOf poluxClienteApp.service:CONF
+ * @description
+ * Variables de configuración de preproducción - pruebas en la nube
+ */
+var conf_cloud_preproduccion = {
+    ACADEMICA_SERVICE: "http://jbpm.udistritaloas.edu.co:8280/services/academicaProxy",
+    POLUX_SERVICE: "http://localhost:8085/v1/",
+    POLUX_MID_SERVICE: "http://localhost:8098/v1/",
+    CONFIGURACION_SERVICE: "http://10.20.0.254/configuracion_api/v1/",
+    NUXEO_SERVICE:"https://documental.udistrital.edu.co/nuxeo/",
+    CORE_SERVICE:"http://10.20.0.254/core_api/v1/",
+    OIKOS_SERVICE:"http://10.20.0.254/oikos_api/v1/",
+    SESIONES_SERVICE:"http://localhost:8081/v1/",
+    TOKEN: {
+        AUTORIZATION_URL: "https://autenticacion.udistrital.edu.co/oauth2/authorize",
+        URL_USER_INFO: "https://autenticacion.udistrital.edu.co/oauth2/userinfo",
+        CLIENTE_ID: "pszmROXqfec4pTShgF_fn2DAAX0a",
+        REDIRECT_URL: "https://pruebaspolux.portaloas.udistrital.edu.co",
+        RESPONSE_TYPE: "code",
+        SCOPE: "openid email",
+        BUTTON_CLASS: "btn btn-warning btn-sm",
+        SIGN_OUT_URL: "https://autenticacion.udistrital.edu.co/oidc/logout",
+        SIGN_OUT_REDIRECT_URL: "https://pruebaspolux.portaloas.udistrital.edu.co",
+        SIGN_OUT_APPEND_TOKEN: "true",
+        REFRESH_TOKEN: "https://autenticacion.udistrital.edu.co/oauth2/token",
+        CLIENT_SECRET: "2crHq2IRkFHEVTBfpznLhKHyKVIa"
+    },
+};
+
+/**
  * @ngdoc service
  * @name poluxClienteApp.service:CONF
  * @description
@@ -83,5 +115,5 @@ var conf_local = {
  */
 angular.module('poluxClienteApp')
   .constant('CONF', {
-      GENERAL: conf_local
+      GENERAL: conf_cloud_preproduccion
   });
