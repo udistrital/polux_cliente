9b86ec006dd9918c0b7aff85d87e35e451295a65
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-10-17 19:00:57 -0500

    #26763# Se modifica servicio de menú

--- app/scripts/controllers/menu.js
+++ app/scripts/controllers/menu.js
@@ -18,21 +18,13 @@
  * @property {array} menu_app Menú de las aplicaciones asociadas a la universidad que se muestra en la parte lateral.
  */
 angular.module('poluxClienteApp')
-    .controller('menuCtrl', function (configuracionRequest, $location, $http, $scope, token_service, notificacion, $translate, $route, $mdSidenav) {
-        var paths = [];
-        $scope.language = {
-            es: "btn btn-primary btn-circle btn-outline active",
-            en: "btn btn-primary btn-circle btn-outline"
-        };
-        $scope.token_service = token_service;
-
-        $scope.notificacion = notificacion;
-        $scope.actual = "";
-        $scope.token_service = token_service;
-        $scope.breadcrumb = [];
-        $scope.menu_service = [];
-
-        $scope.menu_app = [{
+.controller('menuCtrl', function($location, $http, $window, $q, $scope, $rootScope, token_service, configuracionRequest, notificacion, $translate, $route, $mdSidenav) {
+    var paths = [];
+    $scope.language = {
+        es: "btn btn-primary btn-circle btn-outline active",
+        en: "btn btn-primary btn-circle btn-outline"
+    };
+    $scope.menu_app = [{
             id: "kronos",
             title: "KRONOS",
             url: "http://10.20.0.254/kronos"
@@ -70,196 +62,217 @@ angular.module('poluxClienteApp')
             title: "SGA",
             url: "http://10.20.0.254/kronos"
         }
-        ];
+    ];
+    $scope.notificacion = notificacion;
+    $scope.actual = "";
+    $scope.token_service = token_service;
+    $scope.breadcrumb = [];
+    $scope.menu_service = [];
+    /*$scope.menu_service = [{ //aqui va el servicio de el app de configuracion
+            "Id": 3,
+            "Nombre": "Seguimiento y control",
+            "Url": "url_nivel_1",
+            "Opciones": [{
+                    "Id": 1,
+                    "Nombre": "Segumiento Financiero",
+                    "Url": "seguimientoycontrol/financiero",
+                    "Opciones": null
+                },
+                {
+                    "Id": 2,
+                    "Nombre": "Segumiento Legal",
+                    "Url": "",
+                    "Opciones": null
+                },
+                {
+                    "Id": 3,
+                    "Nombre": "Segumiento Tecnico",
+                    "Url": "",
+                    "Opciones": null
+                }
+            ]
+        },
+        { //aqui va el servicio de el app de configuracion
+            "Id": 2,
+            "Nombre": "Necesidad",
+            "Url": "url_nivel_1",
+            "Opciones": [{
+                    "Id": 3,
+                    "Nombre": "Gestion de Necesidades",
+                    "Url": "necesidades",
+                    "Opciones": null
+                },
+                {
+                    "Id": 4,
+                    "Nombre": "Generación de solicitudes de necesidad",
+                    "Url": "necesidad/necesidad_externa",
+                    "Opciones": null
+                }
+            ]
+        },
+        { //RP
+            "Id": 1,
+            "Nombre": "RP",
+            "Url": "",
+            "Opciones": [{ //Consulta de solicitud de RP
+                "Id": 1,
+                "Nombre": "Solicitar registro presupuestal",
+                "Url": "rp_solicitud_personas",
+                "Opciones": null
+            }]
+        },
+        {
+            "Id": 6,
+            "Nombre": "Vinculación especial",
+            "Url": "",
+            "Opciones": [{
+                    "Id": 6,
+                    "Nombre": "Gestión de resoluciones",
+                    "Url": "vinculacionespecial/resolucion_gestion",
+                    "Opciones": null
+                },
+                {
+                    "Id": 6,
+                    "Nombre": "Aprobación de resoluciones",
+                    "Url": "vinculacionespecial/resolucion_aprobacion",
+                    "Opciones": null
+                },
+                {
+                    "Id": 6,
+                    "Nombre": "Administración de resoluciones",
+                    "Url": "vinculacionespecial/resolucion_administracion",
+                    "Opciones": null
+                }
+            ]
+        },
+        {
+            "Id": 5,
+            "Nombre": "Plantillas",
+            "Url": "plantillas/lista_plantillas",
+            "Opciones": null
+        }
+    ];*/
+    var recorrerArbol = function(item, padre) {
+        var padres = "";
+        for (var i = 0; i < item.length; i++) {
+            if (item[i].Opciones === null) {
+                padres = padre + " , " + item[i].Nombre;
+                paths.push({
+                    'path': item[i].Url,
+                    'padre': padres.split(",")
+                });
+            } else {
+                recorrerArbol(item[i].Opciones, padre + "," + item[i].Nombre);
+            }
+        }
+        return padres;
+    };
+    $scope.logout = function(){
+        token_service.logout();
+    };
+    if(token_service.live_token()){
+        $scope.token = token_service.getPayload();
+        if (!angular.isUndefined($scope.token.role)){
+            var roles="";
+            if ( typeof $scope.token.role === "object" ) {
+              var rl = [];
+              for (var index = 0; index < $scope.token.role.length; index++) {
+                if ($scope.token.role[index].indexOf("/") < 0 ){
+                  rl.push($scope.token.role[index]);
+                }
+              }
+              roles = rl.toString();
+            } else {
+              roles = $scope.token.role;
+            }
 
-        if (token_service.live_token()) {
-            token_service.token.role.pop();
-            var roles = token_service.token.role.toString();
-            //para agregar menus 
-            roles = roles + ',ADMIN_POLUX';
-            roles = roles.replace('/', '-');
-            //sobrescribir documento
-            //console.log(token_service.token.documento);
-            configuracionRequest.get('menu_opcion_padre/ArbolMenus/' + roles + '/Polux', '')
-                .then(function (response) {
-                    $scope.menu_service = response.data;
-                    if ($scope.menu_service != null) {
-                        recorrerArbol($scope.menu_service, "");
-                    }
-                    update_url();
+            roles = roles.replace(/,/g, '%2C');
+            configuracionRequest.get('menu_opcion_padre/ArbolMenus/' + roles + '/Polux','').then(function(response) {
+                console.log(response);
+                $rootScope.my_menu = response.data;
+
+            })
+            .catch(
+                function(response) {
+                    console.log(response);
+                    $rootScope.my_menu = response.data;
+    
                 });
         }
+    }
+    /*
+    configuracionRequest.get('menu_opcion_padre/ArbolMenus/' + "ADMINISTRADOR_ARGO" + '/Argo', '').then(function(response) {
+        $rootScope.my_menu = response.data;
+      });
+        /*configuracionRequest.update_menu(https://10.20.0.162:9443/store/apis/authenticate response.data);
+        $scope.menu_service = configuracionRequest.get_menu();*/
 
-        /**
-         * @ngdoc method
-         * @name recorrerArbol
-         * @methodOf poluxClienteApp.controller:menuCtrl
-         * @description 
-         * Recorre el árbol que recibe como parametro y busca los paths asociados.
-         * @param {object} item Item que se busca
-         * @param {object} padre Padre del objeto que se busca. 
-         * @returns {Array} Array de padres del arbol de menús.
-         */
-        var recorrerArbol = function (item, padre) {
-            var padres = "";
-            for (var i = 0; i < item.length; i++) {
-                if (item[i].Opciones === null) {
-                    padres = padre + " , " + item[i].Nombre;
-                    paths.push({
-                        'path': item[i].Url,
-                        'padre': padres.split(",")
-                    });
-                } else {
-                    recorrerArbol(item[i].Opciones, padre + "," + item[i].Nombre);
-                }
+
+    var update_url = function() {
+        $scope.breadcrumb = [''];
+        for (var i = 0; i < paths.length; i++) {
+            if ($scope.actual === "/" + paths[i].path) {
+                $scope.breadcrumb = paths[i].padre;
+            } else if ('/' === $scope.actual) {
+                $scope.breadcrumb = [''];
             }
-            return padres;
-        };
-
-        /**
-         * @ngdoc method
-         * @name update_url
-         * @methodOf poluxClienteApp.controller:menuCtrl
-         * @description 
-         * Actualiza la url actual.
-         * @param {undefined} undefined no requiere parametros
-         * @returns {undefined} No retorna ningún valor.
-         */
-        var update_url = function () {
-            $scope.breadcrumb = [''];
-            for (var i = 0; i < paths.length; i++) {
-                if ($scope.actual === "/" + paths[i].path) {
-                    $scope.breadcrumb = paths[i].padre;
-                } else if ('/' === $scope.actual) {
-                    $scope.breadcrumb = [''];
-                }
-            }
-        };
-        recorrerArbol($scope.menu_service, "");
-        paths.push({ padre: ["", "Notificaciones", "Ver Notificaciones"], path: "notificaciones" });
-
-        /**
-         * @ngdoc method
-         * @name havePermission
-         * @methodOf poluxClienteApp.controller:menuCtrl
-         * @description 
-         * Verifica si un usuario tiene permiso para acceder o no.
-         * @param {string} viewPath Menú al que el usuario quiere acceder.
-         * @param {object} menu Menú de aplicaciones a las que tiene acceso el usuario.
-         * @returns {number} Valor que indica si el usuario tiene permiso o no.
-         */
-        $scope.havePermission = function (viewPath, menu) {
-            var currentPath = viewPath.replace(".html", "").split("views/").pop();
-            var head = menu;
-            var permission = 0;
-            if (currentPath !== "main") {
-                permission = $scope.menuWalkThrough(head, currentPath);
-            } else {
-                permission = 1;
-            }
-            return permission;
-
-        };
-
-        /**
-         * @ngdoc method
-         * @name menuWalkThrough
-         * @methodOf poluxClienteApp.controller:menuCtrl
-         * @description 
-         * Recorre un árbol buscando una url especifica.
-         * @param {object} head Objeto de tipo nodo donde se busca la url.
-         * @param {url} url Url que se busca en el árbol.
-         * @returns {number} Valor que indica si la url se encuetra o no en el árbol.
-         */
-        $scope.menuWalkThrough = function (head, url) {
-            var acum = 0;
-            if (!angular.isUndefined(head)) {
-                angular.forEach(head, function (node) {
-                    if (node.Opciones === null && node.Url === url) {
-                        acum = acum + 1;
-                    } else if (node.Opciones !== null) {
-                        acum = acum + $scope.menuWalkThrough(node.Opciones, url);
-                    } else {
-                        acum = acum + 0;
-                    }
-                });
-                return acum;
-            } else {
-                return acum;
-            }
-        };
-
-        /**
-         * @ngdoc method
-         * @name routeChange
-         * @methodOf poluxClienteApp.controller:menuCtrl
-         * @description 
-         * Escucha los cambios de la ruta y verifica si el usuario tiene permisos para acceder
-         * o no  a la ruta.
-         * @param {object} next Objeto que se captura del evento e identifica la página donde el usuario trata de acceder.  
-         * @returns {undefined} No retorna ningún valor.
-         */
-        $scope.$on('$routeChangeStart', function (scope, next, current) {
-            //Si tiene un Token
-            if (token_service.live_token()) {
-                //Se consultan los menus disponibles para el rol
-                token_service.token.role.pop();
-                var roles = token_service.token.role.toString();
-                //para agregar menus 
-                roles = roles + ',ADMIN_POLUX';
-                //eliminar rol Internal/everyone por error que causa en url
-                roles = roles.replace('/', '-');
-                //sobrescribir documento
-                //console.log(token_service.token.documento);
-                configuracionRequest.get('menu_opcion_padre/ArbolMenus/' + roles + '/Polux', '')
-                    .then(function (response) {
-                        $scope.menu_service = response.data;
-                        //Si no tiene permiso para ingresar al menu es redirigido               
-                        if (!$scope.havePermission(next.templateUrl, $scope.menu_service)) {
-                            $location.path("/no_permission");
-                        }
-                    });
-            } else {
-                //Si no tiene token se dirige a la página de inicios
-                $location.path("/");
-            }
-        });
-
-
-        $scope.changeLanguage = function (key) {
-            $translate.use(key);
-            switch (key) {
-                case 'es':
-                    $scope.language.es = "btn btn-primary btn-circle btn-outline active";
-                    $scope.language.en = "btn btn-primary btn-circle btn-outline";
-                    break;
-                case 'en':
-                    $scope.language.en = "btn btn-primary btn-circle btn-outline active";
-                    $scope.language.es = "btn btn-primary btn-circle btn-outline";
-                    break;
-                default:
-            }
-            $route.reload();
-        };
-
-        function buildToggler(componentId) {
-            return function () {
-                $mdSidenav(componentId).toggle();
-            };
         }
-        $scope.toggleLeft = buildToggler('left');
-        $scope.toggleRight = buildToggler('right');
+    };
 
+    $scope.redirect_url = function(path) {
+        var path_sub = path.substring(0, 4);
+        switch (path_sub.toUpperCase()) {
+            case "HTTP":
+                $window.open(path, "_blank");
+                break;
+            default:
+                $location.path(path);
+                break;
+        }
+    };
 
-        //Pendiente por definir json del menu
-        (function ($) {
-            $(document).ready(function () {
-                $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function (event) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                    $(this).parent().siblings().removeClass('open');
-                    $(this).parent().toggleClass('open');
-                });
-            });
-        })(jQuery);
+    recorrerArbol($scope.menu_service, "");
+    paths.push({ padre: ["", "Notificaciones", "Ver Notificaciones"], path: "notificaciones" });
+
+    $scope.$on('$routeChangeStart', function( /*next, current*/ ) {
+        $scope.actual = $location.path();
+        update_url();
     });
+
+    $scope.changeLanguage = function(key) {
+        $translate.use(key);
+        switch (key) {
+            case 'es':
+                $scope.language.es = "btn btn-primary btn-circle btn-outline active";
+                $scope.language.en = "btn btn-primary btn-circle btn-outline";
+                break;
+            case 'en':
+                $scope.language.en = "btn btn-primary btn-circle btn-outline active";
+                $scope.language.es = "btn btn-primary btn-circle btn-outline";
+                break;
+            default:
+        }
+        $route.reload();
+    };
+
+    function buildToggler(componentId) {
+        return function() {
+            $mdSidenav(componentId).toggle();
+        };
+    }
+
+    $scope.toggleLeft = buildToggler('left');
+    $scope.toggleRight = buildToggler('right');
+
+    //Pendiente por definir json del menu
+    (function($) {
+        $(document).ready(function() {
+            $('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+                event.preventDefault();
+                event.stopPropagation();
+                $(this).parent().siblings().removeClass('open');
+                $(this).parent().toggleClass('open');
+            });
+        });
+    })(jQuery);
+});
