4344a4aa3b591ba84adb964159a0f66c0cca2eb7
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-10-31 09:22:40 -0500

    Se corrige uso de injector

--- app/scripts/services/implicit_token.js
+++ app/scripts/services/implicit_token.js
@@ -8,7 +8,7 @@
  * Service in the implicitToken.
  */
 angular.module('implicitToken', [])
-  .factory('token_service', function($q, CONF, md5, $interval, $injector) {
+  .factory('token_service', function($q, CONF, md5, $interval, autenticacionMidRequest) {
 
     // First, parse the query string
     if (window.localStorage.getItem('access_token') === null ||
@@ -39,7 +39,7 @@ angular.module('implicitToken', [])
           email: appUserInfo.sub
         };
         console.log(emailInfo);
-        $injector.get('autenticacionMidRequest').post("token/emailToken", emailInfo,{ headers :{
+        autenticacionMidRequest.post("token/emailToken", emailInfo,{ headers :{
           'Accept': 'application/json',
           "Authorization": "Bearer " + window.localStorage.getItem('access_token'),
         }})
