9ff8f8af9211cfbfa4e5bf5f0054375725398f65
JuanPiedrahita <juangonzalez1597@gmail.com>
2018-12-04 15:14:18 -0500

    #27718# Se agrega el campo  nivel en carrera elegible y se corrige busqueda de fechas de posgrado y pregrado

--- app/scripts/controllers/materias_profundizacion/publicar_asignaturas.js
+++ app/scripts/controllers/materias_profundizacion/publicar_asignaturas.js
@@ -1,52 +1,112 @@
 'use strict';
 
 /**
- * @ngdoc function
+ * @ngdoc controller
  * @name poluxClienteApp.controller:MateriasProfundizacionPublicarAsignaturasCtrl
  * @description
  * # MateriasProfundizacionPublicarAsignaturasCtrl
  * Controller of the poluxClienteApp
- * Actualmente no está siendo utilizado este controlador, dado que correspondie a la modalidad de materias de profundización
+ * Controlador de la vista publicar asignaturas que permite al cooridnador de posgrado publicar las asignaturas que se ofertarán en el periodo académico siguiente.
+ * @requires $scope 
+ * @requires decorators/poluxClienteApp.decorator:TextTranslate
+ * @requires services/academicaService.service:academicaRequest
+ * @requires services/poluxClienteApp.service:tokenService
+ * @property {Object} periodo Almacena el periodo académico actual
+ * @property {String} modalidad Texto que carga el tipo de modalidad asociada a la publicación de asignaturas
+ * @property {Object} carreras Contiene las carreras asociadas al coorinador
+ * @property {Object} carrera Carrera seleccionada por el coordinador
+ * @property {Object} pensums Pensums asociados a la carrera escogidas
+ * @property {String} mensajeErrorCarga Texto que aparece cuando ocurre un error al cargar los parámetros
+ * @property {Boolean} errorCargarParametros Indicador que maneja la aparición de un error al cargar los parámetros
  */
 angular.module('poluxClienteApp')
   .controller('MateriasProfundizacionPublicarAsignaturasCtrl',
-    function(academicaRequest, $scope, $translate) {
+    function($scope, $translate, academicaRequest, token_service) {
       var ctrl = this;
-      $scope.userId = "60261576";
+      //token_service.token.documento = "12237136";
+      //$scope.userId = token_service.token.documento;
+      $scope.userId = token_service.getAppPayload().appUserDocument;
       ctrl.periodo = [];
       ctrl.modalidad = "PREGRADO";
-      $scope.pensumSeleccionado = null;
+      $scope.msgCargandoSolicitudes = $translate.instant('LOADING.CARGANDO_ASIGNATURAS');
 
-      academicaRequest.get("periodo_academico", "X").then(function(response) {
-        if (!angular.isUndefined(response.data.periodoAcademicoCollection.periodoAcademico)) {
-          ctrl.periodo = response.data.periodoAcademicoCollection.periodoAcademico[0];
-        }
-      });
-
+      /**
+       * @ngdoc method
+       * @name watch
+       * @methodOf poluxClienteApp.controller:MateriasPosgradoPublicarAsignaturasCtrl
+       * @description 
+       * Función de la vista que vigila los cambios en el userId y carga las asignaturas y el periodo académico consumiendo los servicios correspodientes a coordinador_carrera y periodo_academico de {@link services/academicaService.service:academicaRequest academicaRequest}.
+       * @param {undefined} undefined No requiere parámetros
+       * @returns {undefined} Función sin retorno
+       */
       $scope.$watch("userId", function() {
-        $scope.msgCargandoSolicitudes = $translate.instant('LOADING.CARGANDO_ASIGNATURAS');
         $scope.load = true;
         ctrl.carreras = [];
         academicaRequest.get("coordinador_carrera", [$scope.userId, "PREGRADO"]).then(function(response) {
-          console.log(response);
-          if (!angular.isUndefined(response.data.coordinadorCollection.coordinador)) {
-            ctrl.carreras = response.data.coordinadorCollection.coordinador;
-          }
-        });
-        $scope.load = false;
+            console.log(response);
+            if (!angular.isUndefined(response.data.coordinadorCollection.coordinador)) {
+              ctrl.carreras = response.data.coordinadorCollection.coordinador;
+              academicaRequest.get("periodo_academico", "X").then(function(response) {
+                  if (!angular.isUndefined(response.data.periodoAcademicoCollection.periodoAcademico)) {
+                    ctrl.periodo = response.data.periodoAcademicoCollection.periodoAcademico[0];
+                  } else {
+                    ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGANDO_PERIODO');
+                    ctrl.errorCargarParametros = true;
+                  }
+                  $scope.load = false;
+                })
+                .catch(function(error) {
+                  console.log(error);
+                  ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGANDO_PERIODO');
+                  ctrl.errorCargarParametros = true;
+                  $scope.load = false;
+                });
+            } else {
+              ctrl.mensajeErrorCarga = $translate.instant('NO_CARRERAS_PREGADO');
+              ctrl.errorCargarParametros = true;
+              $scope.load = false;
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+            ctrl.mensajeErrorCarga = $translate.instant('ERROR.CARGAR_CARRERAS');
+            ctrl.errorCargarParametros = true;
+            $scope.load = false;
+          });
       });
 
+      /**
+       * @ngdoc method
+       * @name getPensums
+       * @methodOf poluxClienteApp.controller:MateriasPosgradoPublicarAsignaturasCtrl
+       * @description 
+       * Función que es llamada cuando se selecciona una carrera en la vista, consulta los pensums de la carrera consulando el servicio pensums de {@link services/academicaService.service:academicaRequest academicaRequest}.
+       * @param {undefined} undefined No requiere parámetros
+       * @returns {undefined} Función sin retorno
+       */
       ctrl.getPensums = function(carreraSeleccionada) {
-        ctrl.pensums = [];
-        $scope.msgCargandoPensums = $translate.instant('LOADING.CARGANDO_PENSUMS');
         $scope.load = true;
+        $scope.pensumSeleccionado = null;
+        $scope.msgCargandoPensums = $translate.instant('LOADING.CARGANDO_PENSUMS');
+        ctrl.pensums = [];
         academicaRequest.get("pensums", [carreraSeleccionada]).then(function(response) {
-          if (!angular.isUndefined(response.data.pensums.pensum)) {
-            ctrl.carrera = carreraSeleccionada;
-            ctrl.pensums = response.data.pensums.pensum;
-            ctrl.pensumSeleccionado = null;
+            if (!angular.isUndefined(response.data.pensums.pensum)) {
+              ctrl.carrera = carreraSeleccionada;
+              ctrl.pensums = response.data.pensums.pensum;
+              ctrl.pensumSeleccionado = null;
+              $scope.load = false;
+            } else {
+              ctrl.mensajeCargaPensum = $translate.instant('NO_PENSUMS');
+              ctrl.errorCargarPensum = true;
+              $scope.load = false;
+            }
+          })
+          .catch(function(error) {
+            console.log(error);
+            ctrl.mensajeCargaPensum = $translate.instant('ERROR.CARGAR_CARRERAS');
+            ctrl.errorCargarPensum = true;
             $scope.load = false;
-          }
-        });
+          });
       };
+
     });
