41c377e580b26755d1f034fbac38de9502d0c1d2
mantnicot <aldormilon@gmail.com>
2021-09-06 15:23:54 -0500

    Fix: Se arreglo la consulta al Crud de limit y se elimino log

--- app/scripts/controllers/administracion/parametrizacion.js
+++ app/scripts/controllers/administracion/parametrizacion.js
@@ -118,7 +118,6 @@ angular.module('poluxClienteApp')
         ctrl.TablasColeccion = [];
         ctrl.gridOptions2.data = ctrl.TablasColeccion;
         $scope.loadATablasColección = true;
-        console.log('hh');
         poluxRequest.get(tabla.name,'limit=100')
           .then(function(responsetabla) {
             if (Object.keys(responsetabla.data[0]).length > 0) {
