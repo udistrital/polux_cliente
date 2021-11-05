8dbdb257e69f78eff7335c057a142c914dbeee5b
mantnicot <aldormilon@gmail.com>
2021-11-05 08:52:45 -0500

    fix: se arreglo el drone para el build , partes de vulnerabilidaddes y cambio de core amazon crud

--- app/scripts/directives/reportes/reportes_spagobi.js
+++ app/scripts/directives/reportes/reportes_spagobi.js
@@ -19,14 +19,14 @@ angular.module('poluxClienteApp')
       template: '<div id="frame" ></div>',
       controller:function($scope){
         $scope.$watch('boton', function (oldValue,newValue) {
-          if ($scope.codigo && $scope.codigo.length !== 0 && $scope.periodo && $scope.periodo.length !== 0) {
+       //   if ($scope.codigo && $scope.codigo.length !== 0 && $scope.periodo && $scope.periodo.length !== 0) {
             var parametros = '';
             parametros = 'codigo_carrera=' + $scope.codigo + '&Periodo=' + $scope.periodo;       
             url="https://inteligenciainstitucional.portaloas.udistrital.edu.co/knowage/servlet/AdapterHTTP?ACTION_NAME=EXECUTE_DOCUMENT_ACTION&TOOLBAR_VISIBLE=true&ORGANIZATION=DEFAULT_TENANT&NEW_SESSION=true&OBJECT_LABEL="+ReportePoluxG
             $('#frame').html('');
             $('#frame').append(url);
           
-        }
+       // }
       });
     },
     controllerAs:'d_reportesSpagobi'
