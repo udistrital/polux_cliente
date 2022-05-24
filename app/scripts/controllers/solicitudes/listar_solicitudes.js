0a876b693a651c983bdac3de7e4e28d03c9fb2b2
FelipeVel <fvelandia695@gmail.com>
2022-05-24 10:10:34 -0500

    fix: Corrección de url de documentos

--- app/scripts/controllers/solicitudes/listar_solicitudes.js
+++ app/scripts/controllers/solicitudes/listar_solicitudes.js
@@ -546,7 +546,6 @@ angular.module('poluxClienteApp')
                       order: "asc",
                       limit: 1,
                     });
-                    
                     poluxRequest.get("usuario_solicitud", parametrosUsuario).then(function(usuario) { 
                         ctrl.obtenerEstudiantes(solicitud, usuario).then(function(codigo_estudiante) {
                          
@@ -638,6 +637,7 @@ angular.module('poluxClienteApp')
                 ctrl.carrerasCoordinador = responseCoordinador.data.coordinadorCollection.coordinador;
                 angular.forEach(responseCoordinador.data.coordinadorCollection.coordinador, function(carrera) {
                   carreras.push(carrera.codigo_proyecto_curricular);
+                  //carreras.push('20')
                 });
 
                 poluxRequest.get("respuesta_solicitud", parametrosSolicitudes).then(function(responseSolicitudes) {
@@ -662,14 +662,12 @@ angular.module('poluxClienteApp')
                         order: "asc",
                         limit: 1,
                       });
-                      
                       poluxRequest.get("usuario_solicitud", parametrosUsuario).then(function(usuario) { 
                        
                           ctrl.obtenerEstudiantes(solicitud, usuario).then(function(codigo_estudiante) {
                               academicaRequest.get("datos_basicos_estudiante",[codigo_estudiante]).then(function(response2) {
                                   if (!angular.isUndefined(response2.data.datosEstudianteCollection.datosBasicosEstudiante)) {
                                     var carreraEstudiante = response2.data.datosEstudianteCollection.datosBasicosEstudiante[0].carrera;
-                              
                                     if (carreras.includes(carreraEstudiante)) {
                                       solicitud.data.Estado = solicitud.EstadoSolicitud.Nombre;
                                       
