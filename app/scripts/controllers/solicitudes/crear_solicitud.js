e5c5fa49908afc97096b10b0c15be39e76570d44
Andrés Felipe Molano Sánchez <andresfms1@hotmail.com>
2024-07-30 08:55:10 -0500

    fix: se realiza cambio para solución de detalles no obligatorios

--- app/scripts/controllers/solicitudes/crear_solicitud.js
+++ app/scripts/controllers/solicitudes/crear_solicitud.js
@@ -2412,7 +2412,7 @@ angular.module('poluxClienteApp')
           if (detalle.Detalle.CodigoAbreviacion === "DAR" && detalle.fileModel === null) {   
             return;
           }
-          if (detalle.Requerido === false && detalle.respuesta === undefined) {
+          if (detalle.Requerido === false && detalle.respuesta === "undefined") {
             return;
           }
           if (detalle.Id == ctrl.posDocente) {
