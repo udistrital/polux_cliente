300f70b5e435171b588a2e2e9fbe4e728df562e8
JDiegoHernandezA <jdiegohernandeza@gmail.com>
2024-08-08 17:48:05 -0500

    fix: Arreglo de cargue de documentos en Solicitud Inicial #662

--- app/scripts/controllers/solicitudes/crear_solicitud.js
+++ app/scripts/controllers/solicitudes/crear_solicitud.js
@@ -2174,25 +2174,57 @@ angular.module('poluxClienteApp')
         }
       }
 
-      async function uploadDocument(data) {
-        return new Promise(async (resolve, reject) => {
-          await gestorDocumentalMidRequest.post('/document/uploadAnyFormat', data)
-          .then(function (response) {
-             if (response.data.res.Enlace) {
-              let URL = response.data.res.Enlace;
-              console.log("URL del documento:", URL);
-              resolve(URL);
-            } else {
-              reject("No se recibió un enlace en la respuesta.");
-            }
-          })
-          .catch(function (error) {
-            console.error("Error al subir el documento:", error);
-            reject(error);
-          });
-        });
-      }
+      function cargueDocumentos(docs) {//Función para el cargue de documentos al Gestor Documental
 
+        let detalle = docs.shift();//Se toma el primer documento del Array de documentos
+
+        var descripcion;
+        var fileBase64;
+        var data = [];
+        let TipoDocumentoTemp = ctrl.TiposDocumento.find(data => {
+          return data.CodigoAbreviacion == "DTR_PLX"
+        });
+        descripcion = detalle.Detalle.Nombre + ":" + ctrl.codigo;
+        utils.getBase64(detalle.fileModel).then(
+          async function (base64) {
+            fileBase64 = base64;
+
+            //Se prepara la información a enviar a la API
+            data = [{
+              IdTipoDocumento: TipoDocumentoTemp.Id, //id tipo documento de documentos_crud
+              nombre: detalle.fileModel.name, // nombre formado por nombre de la solicitud
+              metadatos: {
+                NombreArchivo: detalle.Detalle.Nombre + ": " + ctrl.codigo,
+                Tipo: "Archivo",
+                Observaciones: "Solicitud inicial"
+              },
+              descripcion: descripcion,
+              file: fileBase64,
+            }]
+
+            await gestorDocumentalMidRequest.post('/document/uploadAnyFormat', data) //Se envía el documento al Gestor Documental
+              .then(function (response) {
+                if (response.data.res.Enlace) {//Si la respuesta fue correcta
+                  detalle.respuesta = response.data.res.Enlace;//Se almacena la URL del documento
+                  ctrl.url = response.data.res.Enlace;
+
+                  if (docs.length > 0) {//Si aún quedan documentos por cargar, se llama a esta misma función y se repite el proceso
+                    cargueDocumentos(docs)
+                  }
+                  else {//Si no quedan más documentos por cargar...
+                    ctrl.cargarSolicitudes();
+                  }
+
+                } else {//El API no retornó la URL del documento
+                  reject("No se recibió un enlace en la respuesta.");
+                }
+              })
+              .catch(function (error) {//El API no respondió de forma correcta
+                console.error("Error al subir el documento:", error);
+                reject(error);
+              });
+          })
+      }
 
       /**
        * @ngdoc method
@@ -2214,124 +2246,12 @@ angular.module('poluxClienteApp')
             var tam = parseInt(detalle.Detalle.Descripcion.split(";")[1] + "000");            
             if (!validExtensions.includes(documento.type) || documento.size > tam) {             
               fileTypeError = true;
-            }
-            // if (detalle.Detalle.Id === 59) {
-            //   console.log("entra a pdf");
-            //   console.log("documento type", documento.type);
-            //   if (documento.type !== "application/pdf" || documento.size > tam) {
-            //     fileTypeError = true;
-            //     console.log("entra pdf ", fileTypeError)
-            //   }
-            // } else if (detalle.Detalle.Id === 82) {
-            //   console.log("entra a rar");
-            //   console.log("documento type", documento.type);
-            //   if (documento.type !== "application/x-zip-compressed" || documento.size > tam) {
-            //     fileTypeError = true;
-            //     console.log("entra zip ", fileTypeError)
-            //   }
-            // }            
+            }            
           });
           $scope.loadFormulario = true;
-          //console.log("file ", fileTypeError)
           if (!fileTypeError) {
 
-            /*let cargueCorrecto = 0
-
-            for (const detalle of ctrl.detallesConDocumento) {
-              var descripcion;
-              var fileBase64;
-              var data = [];
-              let TipoDocumentoTemp = ctrl.TiposDocumento.find(data => {
-                return data.CodigoAbreviacion == "DTR_PLX"
-              });
-              descripcion = detalle.Detalle.Nombre + ":" + ctrl.codigo;
-              utils.getBase64(detalle.fileModel).then(
-                async function (base64) {
-                  fileBase64 = base64;
-                  data = [{
-                    IdTipoDocumento: TipoDocumentoTemp.Id, //id tipo documento de documentos_crud
-                    nombre: detalle.fileModel.name, // nombre formado por nombre de la solicitud
-                    metadatos: {
-                      NombreArchivo: detalle.Detalle.Nombre + ": " + ctrl.codigo,
-                      Tipo: "Archivo",
-                      Observaciones: "Solicitud inicial"
-                    },
-                    descripcion: descripcion,
-                    file: fileBase64,
-                  }]
-                  //console.log("Base64", fileBase64);
-                  await uploadDocument(data).then((res) => {
-                    console.log("Documento subido con éxito:", res);
-                    detalle.respuesta = res
-                    ctrl.url = res
-                    cargueCorrecto++
-                  })
-                    .catch((error) => {
-                      console.error("Error en la subida del documento:", error);
-                    });
-                })
-            }
-
-            console.log(cargueCorrecto)
-            console.log(ctrl.detallesConDocumento.length)
-
-            if(cargueCorrecto == ctrl.detallesConDocumento.length){
-              ctrl.cargarSolicitudes();
-            }*/
-
-            var promiseArray = []
-            ctrl.detallesConDocumento.map((detalle) => {
-              //carga de documentos por el Gestor documental
-              promiseArray.push(new Promise((resolve, reject) => {
-                var URL = "";
-                var descripcion;
-                var fileBase64;
-                var data = [];
-                let TipoDocumentoTemp = ctrl.TiposDocumento.find(data => {
-                  return data.CodigoAbreviacion == "DTR_PLX"
-                });
-                descripcion = detalle.Detalle.Nombre + ":" + ctrl.codigo;
-                utils.getBase64(detalle.fileModel).then(
-                  async function (base64) {
-                    fileBase64 = base64;
-                    data = [{
-                      IdTipoDocumento: TipoDocumentoTemp.Id, //id tipo documento de documentos_crud
-                      nombre: detalle.fileModel.name, // nombre formado por nombre de la solicitud
-                      metadatos: {
-                        NombreArchivo: detalle.Detalle.Nombre + ": " + ctrl.codigo,
-                        Tipo: "Archivo",
-                        Observaciones: "Solicitud inicial"
-                      },
-                      descripcion: descripcion,
-                      file: fileBase64,
-                    }]
-                    //console.log("Base64", fileBase64);
-
-                    await uploadDocument(data).then((res) => {
-                      console.log("Documento subido con éxito:", res);
-                      detalle.respuesta = res
-                      ctrl.url = res
-                      
-                      if (res) {
-                        resolve("Posted");
-                      }
-                    }).catch((error) => {
-                        console.error("Error en la subida del documento:", error);
-                      });
-                    
-                  })
-              }))
-            });
-            Promise.all(promiseArray).then(function (resultado) {
-              ctrl.cargarSolicitudes();
-            }).catch(function (error) {
-              console.log(error)
-              swal(
-                $translate.instant("ERROR.CARGA_SOLICITUDES"),
-                $translate.instant("ERROR.ENVIO_SOLICITUD"),
-                'warning'
-              )
-            })
+            cargueDocumentos(ctrl.detallesConDocumento)
 
           } else {
             swal(
