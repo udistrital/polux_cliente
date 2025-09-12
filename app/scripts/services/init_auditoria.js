a58b2f626c4d7a8a19bae4cf20d81c0676ffa2de
JulianAvella <julianavella01@gmail.com>
2025-09-12 15:06:57 -0500

    fix: Se corrige el error para visualizar el despliegue del microfrontend de auditoria, que tenga permisos para listar las apis que tiene acceso y error de cdigo de una version mayor (udistrital/sga_documentacion#630)

--- app/scripts/services/init_auditoria.js
+++ app/scripts/services/init_auditoria.js
@@ -1,22 +1,3 @@
-/*
-(function () {
-  angular.element(document).ready(function () {
-    try {
-      var injector = angular.element(document.body).injector();
-      var CONF = injector.get("CONF");
-
-      const urlAuditoria = CONF.GENERAL.AUDITORIA;
-      const script = document.createElement("script");
-      script.src = urlAuditoria;
-      script.setAttribute("data-mf", "auditoria");
-      document.body.appendChild(script);
-    } catch (e) {
-      console.error("Error cargando auditoría:", e);
-    }
-  });
-})();
-*/
-
 angular.module('poluxClienteApp')
   .run(['CONF', function(CONF) {
     var script = document.createElement("script");
