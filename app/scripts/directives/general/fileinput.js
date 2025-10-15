300b918ece9c4160cd0a51b0d6aeded5d088cd70
JulianAvella <julianavella01@gmail.com>
2025-10-15 11:24:47 -0500

    feat: Correccion de visualizacion y funcionamiento del campo fille (udistrital/sga_documentacion#642)

--- app/scripts/directives/general/fileinput.js
+++ app/scripts/directives/general/fileinput.js
@@ -1,19 +1,8 @@
 angular.module('poluxClienteApp')
-.directive('fileinput', function($timeout) {
+.directive('fileinput', function() {
   return {
     restrict: 'A',
-    link: function(scope, element, attrs) {
-      $timeout(function() {
-        // Configuración base
-        const options = scope.$eval(attrs.fileinput) || {};
-        $(element).fileinput(Object.assign({
-          showUpload: false,
-          showCaption: true,
-          browseClass: "btn btn-primary",
-          theme: 'explorer-fa',
-          language: 'es'
-        }, options));
-      });
+    link: function(scope, element) {
     }
   };
 });
